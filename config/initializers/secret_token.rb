f7eeb7e20139e89ca2158312b7a3887fc1b13cea
udaya prakash <ud@thetraces.com>
2014-03-11 01:14:31 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ayadu::Application.config.secret_key_base = 'eff72159a74702c72833de62fdedbcc04c88505c25c582f49a8f8376ecb516e1f5d60175cecc76c5d5e72a227f167bf49d1f6b8229f8bca5ac60a6798e714814'
